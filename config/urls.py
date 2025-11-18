53ef12b778cd4003621e1a443dc0797f64027710
JaimeMVC <118700124+JaimeMVC@users.noreply.github.com>
2025-11-18 12:01:22 -0300

    Add files via upload

--- config/urls.py
+++ config/urls.py
@@ -12,6 +12,9 @@ urlpatterns = [
     # Trabajo sobre PN
     path('material/<str:pn>/', views.listado_ubicaciones, name='listado_ubicaciones'),
     path('material/<str:pn>/historial/', views.historial_pn, name='historial_pn'),
+    path('sesion/<int:session_id>/informe/', views.informe_sesion, name='informe_sesion'),
+    path('sesion/<int:session_id>/export-csv/', views.exportar_sesion_csv, name='exportar_sesion_csv'),
+
 
     # Ajax del checklist
     path('toggle-check/', views.toggle_check, name='toggle_check'),
